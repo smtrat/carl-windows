94e1b14b3547addda5e631e68aebad9614a2dc3d
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-11-05 10:11:24 +0100

    Using choco install -y.

--- .ci/setup_appveyor.bat
+++ .ci/setup_appveyor.bat
@@ -1,5 +1,5 @@
-chocolatey install visualstudio2017-workload-vctools
-chocolatey install boost-msvc-12
+choco install -y visualstudio2017-workload-vctools
+choco install -y boost-msvc-12
 
 echo "Looking for preinstalled Boost"
 $env:PATH += ";$BOOST_ROOT"
