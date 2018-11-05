f8bf16270eff339a9237ead3c855fdbffe871123
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-11-05 10:51:23 +0100

    removed debug.

--- .ci/setup_appveyor.bat
+++ .ci/setup_appveyor.bat
@@ -8,7 +8,7 @@ rem $env:PATH += ";$BOOST_LIBRARYDIR"
 md build
 cd build
 
-cmake --trace --debug-output -G "Visual Studio 15 2017 Win64" -DBUILD_STATIC="ON" -DBOOST_ROOT="$BOOST_ROOT" -DBOOST_LIBRARYDIR="$BOOST_LIBRARYDIR" -DCMAKE_BUILD_TYPE=DEBUG -DBoost_COMPILER="-vc141" -DLOGGING="ON" ..
+cmake -G "Visual Studio 15 2017 Win64" -DBUILD_STATIC="ON" -DBOOST_ROOT="$BOOST_ROOT" -DBOOST_LIBRARYDIR="$BOOST_LIBRARYDIR" -DCMAKE_BUILD_TYPE=DEBUG -DBoost_COMPILER="-vc141" -DLOGGING="ON" ..
 cmake --build . --config Debug --target lib_carl
 cmake --build . --config Debug --target lib_carl_static
 cmake --build . --config Debug --target all-tests
