4f61640b3438ecc6caff9a639ddfcc0f8e9a8172
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-11-05 11:04:32 +0100

    Debugging.

--- .ci/setup_appveyor.bat
+++ .ci/setup_appveyor.bat
@@ -1,12 +1,11 @@
 choco install -y visualstudio2017-workload-vctools
 choco install -y boost-msvc-12
 
-echo "Looking for preinstalled Boost"
-rem $env:PATH += ";$BOOST_ROOT"
-rem $env:PATH += ";$BOOST_LIBRARYDIR"
-
 md build
 cd build
+
+dir C:\ProgramData\chocolatey\lib\boost-msvc-12
+dir C:\ProgramData\chocolatey\
 
 cmake -G "Visual Studio 15 2017 Win64" -DBUILD_STATIC="ON" -DBOOST_ROOT="$BOOST_ROOT" -DBOOST_LIBRARYDIR="$BOOST_LIBRARYDIR" -DCMAKE_BUILD_TYPE=DEBUG -DBoost_COMPILER="-vc141" -DLOGGING="ON" ..
 cmake --build . --config Debug --target lib_carl
