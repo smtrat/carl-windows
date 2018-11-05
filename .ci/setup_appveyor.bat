c29aa315e83f8761051a5248f256a7381ce1fb88
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-11-05 11:27:43 +0100

    build boost via EP.

--- .ci/setup_appveyor.bat
+++ .ci/setup_appveyor.bat
@@ -1,14 +1,9 @@
 choco install -y visualstudio2017-workload-vctools
-choco install -y boost-msvc-12
 
 md build
 cd build
 
-dir C:\ProgramData\chocolatey\lib\boost-msvc-12
-dir C:\ProgramData\chocolatey\lib
-dir C:\ProgramData\chocolatey
-
-cmake -G "Visual Studio 15 2017 Win64" -DBUILD_STATIC="ON" -DBOOST_ROOT="$BOOST_ROOT" -DBOOST_LIBRARYDIR="$BOOST_LIBRARYDIR" -DCMAKE_BUILD_TYPE=DEBUG -DBoost_COMPILER="-vc141" -DLOGGING="ON" ..
+cmake -G "Visual Studio 15 2017 Win64" -DBUILD_STATIC="ON" -DCMAKE_BUILD_TYPE=DEBUG -DLOGGING="ON" ..
 cmake --build . --config Debug --target lib_carl
 cmake --build . --config Debug --target lib_carl_static
 cmake --build . --config Debug --target all-tests
