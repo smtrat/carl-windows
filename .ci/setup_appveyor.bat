0dcd3fe7ff57a6cc87af7904cb0d0819596719db
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-11-06 15:00:47 +0100

    split into two jobs.

--- .ci/setup_appveyor.bat
+++ .ci/setup_appveyor.bat
@@ -7,17 +7,23 @@ md build
 cd build
 
 cmake -G "Visual Studio 15 2017 Win64" -DBUILD_STATIC="ON" -DCMAKE_BUILD_TYPE=DEBUG -DLOGGING="ON" ..
-echo "+++++ Building Eigen3"
-cmake --build . --config Debug --target Eigen3-EP
-echo "+++++ Building GMP"
-cmake --build . --config Debug --target GMP-EP
-echo "+++++ Building GTest"
-cmake --build . --config Debug --target GTest-EP
-echo "+++++ Building Boost"
-cmake --build . --config Debug --target Boost-EP
-echo "+++++ Building carl"
-cmake --build . --config Debug --target lib_carl
-cmake --build . --config Debug --target lib_carl_static
-cmake --build . --config Debug --target all-tests
-.\bin\Debug\runCoreTests.exe
-cmake --build . --config Debug --target RUN_TESTS
+
+IF /I "%TASK%"=="dependencies" (
+
+	echo "+++++ Building Eigen3"
+	cmake --build . --config Debug --target Eigen3-EP
+	echo "+++++ Building GMP"
+	cmake --build . --config Debug --target GMP-EP
+	echo "+++++ Building GTest"
+	cmake --build . --config Debug --target GTest-EP
+	echo "+++++ Building Boost"
+	cmake --build . --config Debug --target Boost-EP
+
+) ELSE (
+	echo "+++++ Building carl"
+	cmake --build . --config Debug --target lib_carl
+	cmake --build . --config Debug --target lib_carl_static
+	cmake --build . --config Debug --target all-tests
+	.\bin\Debug\runCoreTests.exe
+	cmake --build . --config Debug --target RUN_TESTS
+)
