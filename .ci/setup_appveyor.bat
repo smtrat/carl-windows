940e937f078f74a68bda7f628968130684a8cc81
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-11-06 12:23:08 +0100

    ...

--- .ci/setup_appveyor.bat
+++ .ci/setup_appveyor.bat
@@ -7,10 +7,15 @@ md build
 cd build
 
 cmake -G "Visual Studio 15 2017 Win64" -DBUILD_STATIC="ON" -DCMAKE_BUILD_TYPE=DEBUG -DLOGGING="ON" ..
+echo "+++++ Building Eigen3"
 cmake --build . --config Debug --target Eigen3-EP
+echo "+++++ Building GMP"
 cmake --build . --config Debug --target GMP-EP
+echo "+++++ Building GTest"
 cmake --build . --config Debug --target GTest-EP
+echo "+++++ Building Boost"
 cmake --build . --config Debug --target Boost-EP
+echo "+++++ Building carl"
 cmake --build . --config Debug --target lib_carl
 cmake --build . --config Debug --target lib_carl_static
 cmake --build . --config Debug --target all-tests
